d7b041c17ad1cc691dc1aa74ce05a736f38b0ca5
jiyoung choi <codingold@naver.com>
2024-07-09 03:51:37 +0900

    initial settings

---
+++ src/pages/HomeView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../section/Header'
+import { Main } from '../section/Main'
+import { Footer } from '../section/Footer'
+
+export const HomeView = () => {
+  return (
+    <div>
+    <Header />
+    <Main />
+    <Footer />
+    </div>
+  )
+}
