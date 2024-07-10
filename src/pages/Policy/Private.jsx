7a4ea99ff4fad0d7a307839cca1595484cc998dd
jiyoung choi <codingold@naver.com>
2024-07-10 14:36:10 +0900

    upload Develop

--- src/pages/Policy/Private.jsx
+++ src/pages/Policy/Private.jsx
@@ -6,7 +6,12 @@ export const Private = () => {
     <div className="Policy_pages">
       <h1>개인정보처리방침</h1>
       <br></br>
-      <div className="Policy_des">내용</div>
+      <div className="Policy_des">
+        <img src="Private01.png" alt="이미지 설명" width="800" height="120" />
+        <img src="Private02.png" alt="이미지 설명" width="800" height="550" />
+        <img src="Private03.png" alt="이미지 설명" width="800" height="550" />
+        <img src="Private04.png" alt="이미지 설명" width="800" height="200" />
+      </div>
     </div>
   );
 };
